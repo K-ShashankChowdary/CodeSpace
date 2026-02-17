87406f719ecbd12aaa86b5430fc4d7b570491dc8
Shashank Chowdary <kshashankchowdary14@gmail.com>
2026-02-17 22:35:04 +0530

    initialize frontend with vite,and organized the fronend with a solid folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Workspace from "./components/Workspace";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0f0f0f] text-white">
+       {/* We will load the Workspace component here shortly */}
+       <Workspace />
+    </div>
+  );
+}
+
+export default App;
